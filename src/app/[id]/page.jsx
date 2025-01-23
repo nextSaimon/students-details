93308ec927da67fec0f2b623d31fc37696619263
Saimon <saimonss5432@gmail.com>
2025-01-23 08:25:53 +0600

    realtime db

--- src/app/[id]/page.jsx
+++ src/app/[id]/page.jsx
@@ -1,9 +1,5 @@
 import React from "react";
 import SectionPage from "@/components/page/Section";
-import { connectToDB } from "@/lib/db";
-import Batch from "@/models/hsc";
-import Section from "@/models/Section";
-import { notFound } from "next/navigation";
 
 export default async function Page({ params }) {
 
